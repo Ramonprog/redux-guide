d5adfb0a48733641219569f11dd2d2c62509af58
Ramonprog <programador.ramon@gmail.com>
2024-01-16 08:37:53 -0300

    configurando e usando reducer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // Components
 import Cart from "../cart/index";
@@ -9,18 +10,40 @@ import * as Styles from "./styles";
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
+  const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+  console.log("🚀 ~ Header ~ currentUser:", currentUser);
+  const dispatch = useDispatch();
+
   const handleCartClick = () => {
     setCartIsVisible(true);
+  };
+
+  const handleLoginClick = () => {
+    dispatch({
+      type: "SET_CURRENT_USER",
+      payload: {
+        nome: "Claudio",
+        email: "claudio@teste.com",
+      },
+    });
+  };
+
+  const handleLogoutClick = () => {
+    dispatch({
+      type: "SET_LOGOUT",
+    });
   };
 
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        <div onClick={handleLoginClick}>Login</div>
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
-
+      <Styles.Buttons>
+        <div onClick={handleLogoutClick}>Logout</div>
+      </Styles.Buttons>
       <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
     </Styles.Container>
   );
--- src/redux/user/reducer.js
+++ src/redux/user/reducer.js
@@ -1,3 +1,22 @@
-const userReducer = () => {};
+const initialState = {
+  currentUser: null,
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    case "SET_LOGOUT":
+      return {
+        ...state,
+        currentUser: null,
+      };
+    default:
+      return state;
+  }
+};
 
 export default userReducer;
